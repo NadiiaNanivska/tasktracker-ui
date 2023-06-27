a46e706737a2b55b0dae6b2425133f78219bed54
Vitalik2705 <vitok2misze@gmail.com>
2023-06-27 21:30:55 +0300

    divided all files into folders

---
+++ src/components/Sidebar.js
@@ -0,0 +1,39 @@
+import '../styles/Sidebar.css';
+import React from 'react';
+import HomeIcon from '../images/home.svg';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h1 className="sidebar-heading">TaskTracker</h1>
+            <ul className="sidebar-nav">
+                <li className="sidebar-item">
+                    <a href="#" className="sidebar-link">
+                        <div className="icon-wrapper">
+                            <img src={HomeIcon} alt="Home" className="sidebar-icon" />
+                        </div>
+                        <span className="sidebar-text">Home</span>
+                    </a>
+                </li>
+                <li className="sidebar-item">
+                    <a href="#" className="sidebar-link">
+                        <div className="icon-wrapper">
+                            <img src={HomeIcon} alt="Home" className="sidebar-icon" />
+                        </div>
+                        <span className="sidebar-text">Tasks</span>
+                    </a>
+                </li>
+                <li className="sidebar-item">
+                    <a href="#" className="sidebar-link">
+                        <div className="icon-wrapper">
+                            <img src={HomeIcon} alt="Home" className="sidebar-icon" />
+                        </div>
+                        <span className="sidebar-text">Calendar</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
