c256f150355e9756f85f2a52203c2481c15878b4
Vitalik2705 <vitok2misze@gmail.com>
2023-07-04 21:15:03 +0300

    Added axios

--- src/utils/tasksRequests.js
+++ src/utils/tasksRequests.js
@@ -1,22 +1,13 @@
+import axios from 'axios';
 
 export const addTask = async (title, description, name) => {
-    const myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    const raw = JSON.stringify({
-        title: title,
-        description: description,
-        status: name,
-    });
-
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow',
-    };
-
-    fetch('http://localhost:8080/tasks', requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.log('error', error));
-}
+    try {
+        await axios.post('http://localhost:8080/tasks', {
+            title: title,
+            description: description,
+            status: name,
+        });
+    } catch (error) {
+        console.log('error', error);
+    }
+};
