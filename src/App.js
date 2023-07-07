001d5e927c1de11d98cbb437e98f1dffbad872ea
Vitalik2705 <vitok2misze@gmail.com>
2023-07-07 18:31:36 +0300

    Implemented register page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
          <Routes>
            <Route path="/tasks" element={<Home />}></Route>
              <Route path="/login" element={<Login />}></Route>
+             <Route path="/register" element={<Register />}></Route>
          </Routes>
        </BrowserRouter>
       </>
---
+++ src/components/Register.js
@@ -0,0 +1,39 @@
+import React, { useContext, useState, useEffect } from 'react';
+import '../styles/Register.css';
+import {Button, Input, Checkbox} from 'antd';
+import {PlusOutlined} from "@ant-design/icons";
+const Register = () => {
+    return (
+        <div className="register">
+            <div className="register-block">
+                <div className="register-head">
+                    <div className="register-head-wrapper">
+                        <div className="register-head-name">TaskTracker</div>
+                        <div className="register-head-text">Register</div>
+                    </div>
+                    <div className="register-credentials-wrapper">
+                        <Input size="large" className="register-input" placeholder="First Name" />
+                        <Input size="large" className="login-input" placeholder="Last Name" />
+                        <Input size="large" className="login-input" placeholder="Email" />
+                        <Input size="large" className="login-input" placeholder="Password" />
+                        <Input size="large" className="login-input" placeholder="Repeat password" />
+                        <Input size="large" className="login-input" placeholder="Phone Number" />
+                    </div>
+                    <div className="register-button-wrapper">
+                        <Button type="primary" className="card-add-btn">
+                            SIGN UP
+                        </Button>
+                    </div>
+                    <a href="/login" className="register-without-account">
+                        Already have an account? Log In!
+                    </a>
+                    <div className="register-copyright">
+                        &copy; TaskTracker 2023. All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
