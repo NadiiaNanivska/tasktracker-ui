0bb0b6de3aeb0974ea232cdc2751f4bf3c8726af
Vitalik2705 <vitok2misze@gmail.com>
2023-07-09 15:33:53 +0300

    Fixed bug with dark mode

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -33,36 +33,36 @@ const HomePage = () => {
 
     return (
         <div className={`home-page ${isDarkMode ? 'dark' : ''}`}>
-            <Sidebar updateSidebarWidth={updateSidebarWidth} updateContentWidth={updateContentWidth}/>
+            <Sidebar isDarkMode={isDarkMode} updateSidebarWidth={updateSidebarWidth} updateContentWidth={updateContentWidth}/>
             <div className="home-page-content" ref={contentRef}>
                 <div className="home-page-left">
                     <div className="home-page-left-head">
                         TaskTracker
                     </div>
-                    <div className="home-page-left-middle">
+                    <div className={`home-page-left-middle ${isDarkMode ? 'dark' : ''}`}>
                         TASK MANAGEMENT
                     </div>
                     <div className="home-page-left-end">
                         Make a business plan or list of resolutions to achieve goals or achieve success.
                     </div>
                     <div className="home-page-about">
-                        <p>Contact us:</p>
+                        <p className={`home-page-about-text ${isDarkMode ? 'dark' : ''}`}>Contact us:</p>
                         <div className="social-media-icons">
                             <a href="mailto:vitok2misze@gmail.com">
-                                <img src={mailIcon} alt="Email" className="icon" />
-                                <span>Email</span>
+                                <img src={mailIcon} alt="Email" className="home-page-about-icon" />
+                                <span className={`home-page-about-text ${isDarkMode ? 'dark' : ''}`}>Email</span>
                             </a>
                             <a href="https://www.facebook.com/profile.php?id=100015042850556">
-                                <img src={facebookIcon} alt="Facebook" className="icon" />
-                                <span>Facebook</span>
+                                <img src={facebookIcon} alt="Facebook" className="home-page-about-icon" />
+                                <span className={`home-page-about-text ${isDarkMode ? 'dark' : ''}`}>Facebook</span>
                             </a>
                             <a href="https://www.instagram.com/vitalikyatskiv/">
-                                <img src={instagramIcon} alt="Instagram" className="icon" />
-                                <span>Instagram</span>
+                                <img src={instagramIcon} alt="Instagram" className="home-page-about-icon" />
+                                <span className={`home-page-about-text ${isDarkMode ? 'dark' : ''}`}>Instagram</span>
                             </a>
                             <a href="https://web.telegram.org/k/#@V_love_N">
-                                <img src={telegramIcon} alt="Telegram" className="icon" />
-                                <span>Telegram</span>
+                                <img src={telegramIcon} alt="Telegram" className="home-page-about-icon" />
+                                <span className={`home-page-about-text ${isDarkMode ? 'dark' : ''}`}>Telegram</span>
                             </a>
                         </div>
                     </div>
